2d55e83c5bb2d653551f6d1cd4ca4852de2f6ee4
Anna Gubskaya <gubskaya.ann@gmail.com>
2022-08-30 12:21:22 +0500

    github pages

--- gulpfile.js
+++ gulpfile.js
@@ -338,3 +338,13 @@ exports.build = series(toProd, clean, htmlInclude, scripts, styles, resources, i
 exports.cache = series(cache, rewrite);
 
 exports.zip = zipFiles;
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+ gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
