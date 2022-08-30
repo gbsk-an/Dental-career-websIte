12da990cc798b4e439076cc38e338f9b9212ca11
Anna Gubskaya <gubskaya.ann@gmail.com>
2022-08-30 08:27:18 +0500

    Work on slider

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -2,12 +2,25 @@ import Swiper, { Navigation, Scrollbar } from 'swiper';
 
 Swiper.use([Navigation, Scrollbar]);
 
-const swiper = new Swiper('.hiw__content', {
+const slider = document.querySelector('.hiw__content');
+const slider2 = document.querySelector('.feedback__content');
+
+let swiper = new Swiper(slider, {
   slidesPerView: 2,
   loop: true,
   speed: 600,
   navigation: {
     nextEl: '.hiw__slider-btn--next',
     prevEl: '.hiw__slider-btn--prev',
+  },
+});
+
+let swiper2 = new Swiper(slider2, {
+  slidesPerView: 1,
+  loop: false,
+  speed: 600,
+  navigation: {
+    nextEl: '.hiw__slider-bt--next',
+    prevEl: '.hiw__slider-bt--prev',
   },
 });
